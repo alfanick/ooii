e88cf15e501a8255ae46de7f32d76fca69bbaf20
Amadeusz Juskowiak <amadeusz@amanointeractive.com>
2012-12-15 08:51:00 +0100

    Pusty projekt. NetBeans 7.2.1.

---
+++ src/gomoku/Gomoku.java
@@ -0,0 +1,19 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package gomoku;
+
+/**
+ *
+ * @author alfanick
+ */
+public class Gomoku {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+}
