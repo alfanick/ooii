809ae5ee99900a5409db4525e5b463be88a733d5
Amadeusz Juskowiak <amadeusz.juskowiak@student.put.poznan.pl>
2012-12-27 10:27:46 +0100

    Utworzono wątki dla UI i gry (oraz odpowiednie pola dla nich). Wątki są startowane.

--- src/gomoku/Gomoku.java
+++ src/gomoku/Gomoku.java
@@ -17,6 +17,16 @@ public class Gomoku {
      * Game GUI.
      */
     public static GomokuUI ui;
+    
+    /**
+     * UI Thread
+     */
+    public static Thread uiThread;
+    
+    /**
+     * Game Thread
+     */
+    public static Thread gameThread;
 
     /**
      * Main program loop. Creates game logic and GUI.
@@ -24,6 +34,15 @@ public class Gomoku {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
+        Gomoku.ui = new GomokuUI();
+        Gomoku.uiThread = new Thread(Gomoku.ui);
+        
+        Gomoku.game = new Game();
+        Gomoku.gameThread = new Thread(Gomoku.game);
+        
+        Gomoku.uiThread.start();
+        Gomoku.gameThread.start();
+        
         System.out.println("Hello, World!");
     }
 }
