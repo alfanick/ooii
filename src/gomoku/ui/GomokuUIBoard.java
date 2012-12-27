919a48555f820a7fe115c080afe20bcc5949a25f
ked <jpeikert@gmail.com>
2012-12-27 15:27:29 +0100

    GomokuUIBoard w trakcie zmian i pracy

--- src/gomoku/ui/GomokuUIBoard.java
+++ src/gomoku/ui/GomokuUIBoard.java
@@ -1,7 +1,11 @@
 package gomoku.ui;
 
+import gomoku.GameRules;
 import java.awt.Component;
 import java.awt.Graphics;
+import java.awt.Graphics2D;
+import java.awt.Rectangle;
+import javax.swing.*;
 
 /**
  * Component representing Gomoku board.
@@ -20,7 +24,16 @@ public class GomokuUIBoard extends Component {
      */
     @Override
     public void paint(Graphics g) {
-    
+        super.paint(g);
+        GameRules rules = new GameRules(new Rectangle(0,0,9,9), new Rectangle(3,3,5,5), 5);
+        Rectangle board;
+        Graphics2D g2d = (Graphics2D) g;
+        board = rules.getSizeRectangle();
+        for (int i = 0; i <= board.height; i++) {
+        g2d.drawLine(350, 50 + i*50, 850, 50 + i*50);    
+        }
+        
+        
         throw new UnsupportedOperationException("Not supported yet.");
     }
 }
