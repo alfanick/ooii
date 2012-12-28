68cf9fdbb54f9cc980ad86cb7844d8a3149967a6
Amadeusz Juskowiak <amadeusz.juskowiak@student.put.poznan.pl>
2012-12-28 11:22:59 +0100

    Poprawiono bug w timerze paska - nie działach dobrze dla długich czasów.

--- src/gomoku/ui/GomokuUI.java
+++ src/gomoku/ui/GomokuUI.java
@@ -213,7 +213,7 @@ public class GomokuUI extends JFrame implements Runnable  {
             public void run() {
                 progressbar.setValue(Math.min(progressbar.getValue() + 1, 100));
             } 
-        }, 0l, Math.round(time*1000)/100l);
+        }, 0l, Math.round(time*10));
     }
     
     /**
